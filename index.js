c4770e5ab3f5e66cce4935324ee78f9ccdaf36fd
eno1220 <eno1220dev@gmail.com>
2023-01-30 02:49:23 +0900

    update: v1.0 -> v1.1

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 async function submit() {
+  const msgSuccess = document.getElementById("msg-success");
+  const msgError = document.getElementById("msg-error");
+  const msgNoUrl = document.getElementById("msg-no-url");
   const memo =
     document.getElementById("memo").value != ""
       ? "【memo】\r" + document.getElementById("memo").value + "\r"
@@ -13,7 +16,7 @@ async function submit() {
     "【URL】\r" +
     tab.url;
 
-  const url = ""; // Webhook URL
+  const url = undefined; // Webhook URL
   const param = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -21,11 +24,26 @@ async function submit() {
       content: message,
     }),
   };
+  if (url == undefined) {
+    msgNoUrl.style.display = "block";
+    setTimeout(() => {
+      msgNoUrl.style.display = "none";
+    }, 3000);
+    return;
+  }
   fetch(url, param).then((response) => {
     if (response.ok) {
       console.log("ok");
+      msgSuccess.style.display = "block";
+      setTimeout(() => {
+        msgSuccess.style.display = "none";
+      }, 3000);
     } else {
-      console.log("error");
+      console.log("error", response);
+      msgError.style.display = "block";
+      setTimeout(() => {
+        msgError.style.display = "none";
+      }, 3000);
     }
   });
 }
